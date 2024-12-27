e3d25c315d4069e2d1026d1ead93aeda1d898ce2
arcane <tuckeralbert.le.e@gmail.com>
2024-12-27 11:39:08 -0600

    Update template.py

--- template/template.py
+++ template/template.py
@@ -8,10 +8,14 @@ port_number = 5000 # port number, 5000 is default
 host_ip = 0.0.0.0 # change to IP if you plan on doing port-forwarding
 # end server setup
 
+# add you api extensions here
 @YOUR_SERVER_NAME.route("/")
-def home():
-   if  is None and 
-   return on_login_message
+def home(): 
+   info = request.args.get("info")
+   if info == sessiontime:
+      return session_time # you would have to have a session_time var defined globally in this case 
+   elif info is None:    # if you have an info parameter then you can recieve info from the server
+      return on_login_message # in this case http://0.0.0.0:5000?info=sessiontime
    
 if __name__ == "__main__":
     print(f"Booting {YOUR_SERVER_NAME} on port {port_number")
