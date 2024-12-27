9878523c317d972176ea1b4d34c03721c81853ac
arcane <tuckeralbert.le.e@gmail.com>
2024-12-27 10:01:50 -0600

    Update template.py

--- template/template.py
+++ template/template.py
@@ -12,7 +12,7 @@ host_ip = 0.0.0.0 # change to IP if you plan on doing port-forwarding
 def home():
    if  is None and 
    return on_login_message
-    
+   
 if __name__ == "__main__":
     print(f"Booting {YOUR_SERVER_NAME} on port {port_number")
     YOUR_SERVER_NAME.run(host=host_ip, port=port_number)
